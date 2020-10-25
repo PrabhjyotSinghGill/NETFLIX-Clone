5822281de17f86ee2f89a92346c1b22987f1836b
Prabhjyot Singh <prabhjyotg@gmail.com>
2020-10-25 17:58:31 -0400

    Updates

---
+++ precache-manifest.1728045ac3f3e77c2ea773dc5d9fce2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7596f60d8d9991c3342df5ddbfdc29aa",
+    "url": "/PrabhjyotSinghGill/NETFLIX-Clone/index.html"
+  },
+  {
+    "revision": "7b0cf75b377eb04b4727",
+    "url": "/PrabhjyotSinghGill/NETFLIX-Clone/static/css/main.aed9d8cf.chunk.css"
+  },
+  {
+    "revision": "12dcbf93bd6457b51b8e",
+    "url": "/PrabhjyotSinghGill/NETFLIX-Clone/static/js/2.2388b70d.chunk.js"
+  },
+  {
+    "revision": "ee4991bbc4f3dcc561e92da49df07222",
+    "url": "/PrabhjyotSinghGill/NETFLIX-Clone/static/js/2.2388b70d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b0cf75b377eb04b4727",
+    "url": "/PrabhjyotSinghGill/NETFLIX-Clone/static/js/main.7458a109.chunk.js"
+  },
+  {
+    "revision": "6c849c3809e2ea3b2942",
+    "url": "/PrabhjyotSinghGill/NETFLIX-Clone/static/js/runtime-main.5d17cbd9.js"
+  }
+]);
